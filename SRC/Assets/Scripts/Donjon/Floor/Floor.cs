e26e73fcbd110e8089f5c3406fa7eaf2b5defcd3
Petiot Thibaut <petiotthib@gmail.com>
2019-08-06 18:44:08 +0200

    wip

--- SRC/Assets/Scripts/Donjon/Floor/Floor.cs
+++ SRC/Assets/Scripts/Donjon/Floor/Floor.cs
@@ -5,27 +5,29 @@ using UnityEngine;
 namespace Donjon
 {
 	public class Floor
-	{
-		private readonly Cell[,] Cells;
-		private readonly int m_yLength;
-		private readonly int m_xLength;
+    {
+        public readonly int YLength;
+        public readonly int XLength;
+
+        private readonly Cell[,] Cells;
 
 		public Floor(Cell[,] cells, int yLength, int xLength)
 		{
 			Cells = cells;
-			m_yLength = yLength;
-			m_xLength = xLength;
+			YLength = yLength;
+			XLength = xLength;
 			Debug.Log("Init Floor");
 		}
 
 		public Cell GetCell(Vector2Int pos)
 		{
-			if (pos.y >= m_yLength || pos.y < 0)
-				return null;
-			if (pos.x >= m_xLength || pos.x < 0)
-				return null;
-			//Debug.LogFormat("GetCell {0} {1} {2}", pos, m_yLength, m_xLength);
-			return Cells[pos.y, pos.x];
+			return IsValidPosition(pos) ? Cells[pos.y, pos.x] : null;
 		}
-	}
+
+        public bool IsValidPosition(Vector2Int posToTest)
+        {
+            return posToTest.y < YLength && posToTest.y >= 0 && posToTest.x < XLength && posToTest.x >= 0;
+        }
+
+    }
 }
--- SRC/Assets/Scripts/Donjon/PathFinding/AStar.cs
+++ SRC/Assets/Scripts/Donjon/PathFinding/AStar.cs
@@ -1,30 +1,70 @@
-﻿using System.Collections;
+﻿using Donjon;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AStar
 {
-    private class Tile
+    private struct Tile
     {
-        public int g;
-        public int h;
-        public int f { get { return g + h; } }
+        public float g, h, f;
 
+        public Vector2Int parentPos;
+    }
+
+    public struct FandPos
+    {
+        public float f;
         public Vector2Int pos;
     }
-    public void Search()
+
+    
+    public void Search(
+        Func<Vector2Int> isCellValid, 
+        Floor floor, 
+        Vector2Int startPos,
+        Vector2Int endPos)
     {
-        List<Tile> openList = new List<Tile>();
-        List<Tile> closeList = new List<Tile>();
+        if (!floor.IsValidPosition(startPos))
+            return;
+
+        if (!floor.IsValidPosition(endPos))
+            return;
+
+        //Test start end blocked
+
+        var closeList = new bool[floor.YLength, floor.XLength];
+        var cellDetails = new Tile[floor.YLength, floor.XLength];
+        for (int i = 0; i < floor.YLength; i++)
+        {
+            for (int j = 0; j < floor.XLength; j++)
+            {
+                cellDetails[i,j].f = float.MaxValue;
+                cellDetails[i,j].g = float.MaxValue;
+                cellDetails[i,j].h = float.MaxValue;
+                cellDetails[i, j].parentPos = new Vector2Int(-1, -1);
+            }
+        }
 
         // add starting node to open list
+
+        var yIndex = startPos.y;
+        var xIndex = startPos.x;
+        cellDetails[yIndex,xIndex].f = 0.0f;
+        cellDetails[yIndex,xIndex].g = 0.0f;
+        cellDetails[yIndex,xIndex].h = 0.0f;
+        cellDetails[yIndex, xIndex].parentPos = startPos;
+
+        var openList = new HashSet<FandPos>();
 
         while (openList.Count != 0)
         {
             //find node with the small f in openList
-            Tile q = new Tile();
-
+            var q = openList.GetEnumerator().Current;
             openList.Remove(q);
+            startPos = q.pos;
+            closeList[startPos.y, startPos.x] = true;
 
             //generate q's 8 successors and set their parents to q
             Tile[] successors = new Tile[8];
@@ -38,13 +78,13 @@ public class AStar
                     return;
                 }
 
-                var hasNodeSamePos = openList.Find(item => item.pos == successor.pos && item.f <= successor.f) != null;
+                var hasNodeSamePos = openList.Find(item => item.pos == successor.parentPos && item.f <= successor.f) != null;
 
                 if (hasNodeSamePos)
                     continue;
 
 
-                hasNodeSamePos = closeList.Find(item => item.pos == successor.pos && item.f <= successor.f) != null;
+                hasNodeSamePos = closeList.Find(item => item.pos == successor.parentPos && item.f <= successor.f) != null;
 
                 if (hasNodeSamePos)
                     continue;
@@ -56,5 +96,53 @@ public class AStar
         }
 
         return;
+    }
+
+    private static bool IsNValid(
+        Floor floor, int yPos, int xPos,
+        Vector2Int currentPos, Vector2Int endPos, 
+        Tile[,] cellDetails, bool[,] closedList,
+        HashSet<FandPos> openList)
+    {
+        var pos = new Vector2Int(xPos, yPos);
+        if (!floor.IsValidPosition(pos))
+            return false;
+
+        // If the destination cell is the same as the 
+        // current successor 
+        if (pos == endPos)
+        {
+            // Set the Parent of the destination cell 
+            cellDetails[pos.y, pos.x].parentPos = currentPos;
+            return true;
+        }
+        // If the successor is already on the closed 
+        // list or if it is blocked, then ignore it. 
+        // Else do the following 
+        if (closedList[pos.y, pos.x] != false || !floor.isUnBlocked(pos))
+            return false;
+
+        var gNew = cellDetails[currentPos.y, currentPos.x].g + 1.0;
+        var hNew = calculateHValue(pos, endPos);
+        var fNew = gNew + hNew;
+
+        // If it isn’t on the open list, add it to 
+        // the open list. Make the current square 
+        // the parent of this square. Record the 
+        // f, g, and h costs of the square cell 
+        //                OR 
+        // If it is on the open list already, check 
+        // to see if this path to that square is better, 
+        // using 'f' cost as the measure. 
+        if (cellDetails[pos.y, pos.x].f != float.MaxValue && cellDetails[pos.y, pos.x].f <= fNew)
+            return false;
+
+        openList.Add(new FandPos() { f = fNew, pos = pos });
+
+        // Update the details of this cell 
+        cellDetails[pos.y, pos.x].f = fNew;
+        cellDetails[pos.y, pos.x].g = gNew;
+        cellDetails[pos.y, pos.x].h = hNew;
+        cellDetails[pos.y, pos.x].parentPos = currentPos;
     }
 }
